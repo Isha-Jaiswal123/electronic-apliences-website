f881123e93ee237569843b6625bad8454ed92f03
Isha-Jaiswal123 <ishujaiswal746@gmail.com>
2024-01-08 15:35:47 +0530

    Add INISIAL FILES

---
+++ index.js
@@ -0,0 +1,33 @@
+function run(){
+    let menu=document.getElementById("menu");
+    let bars=document.getElementById("bar");
+
+    if(menu.style.display !='none'){
+        menu.style.display='none' ;
+    }else{
+        menu.style.display='block';
+    }
+}
+
+function run2(){
+    let searchbar=document.getElementById("searchbar");
+    let search=document.getElementById("search");
+
+    if(searchbar.style.display !='none'){
+        searchbar.style.display='none' ;
+    }else{
+       searchbar.style.display='block';
+    }
+}
+function run3(){
+    let categories=document.getElementById("categories");
+
+
+    if(categories.style.display !='none'){
+        categories.style.display='none' ;
+    }else{
+       categories.style.display='block';
+    }
+}
+
+let shopBtn=document.querySelector()
